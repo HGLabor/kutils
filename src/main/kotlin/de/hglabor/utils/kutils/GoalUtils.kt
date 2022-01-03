4eafd4eb4661306b342ced3a941705df83e13c4d
Krxwallo <ken.grill@gmx.de>
2022-01-03 19:39:05 +0100

    update goal utils

--- src/main/kotlin/de/hglabor/utils/kutils/GoalUtils.kt
+++ src/main/kotlin/de/hglabor/utils/kutils/GoalUtils.kt
@@ -1,9 +1,11 @@
 package de.hglabor.utils.kutils
 
 import com.destroystokyo.paper.entity.ai.Goal
+import com.destroystokyo.paper.entity.ai.GoalKey
 import org.bukkit.Bukkit
 import org.bukkit.entity.Mob
 
 val mobGoals get() = Bukkit.getMobGoals()
 
 fun Mob.addGoal(priority: Int, goal: Goal<Mob>) = mobGoals.addGoal(this, priority, goal)
+fun Mob.hasGoal(priority: Int, key: GoalKey<Mob>) = mobGoals.hasGoal(this, key)
