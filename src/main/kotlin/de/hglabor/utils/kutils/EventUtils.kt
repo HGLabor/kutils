a4e920727c77379657a935540cbe373206121d01
Krxwallo <ken.grill@gmx.de>
2022-01-24 16:58:29 +0100

    add cancelEvent and cancelEventWhen and bump up version to 0.0.16

--- src/main/kotlin/de/hglabor/utils/kutils/EventUtils.kt
+++ src/main/kotlin/de/hglabor/utils/kutils/EventUtils.kt
@@ -1,6 +1,24 @@
 package de.hglabor.utils.kutils
 
+import net.axay.kspigot.event.listen
 import org.bukkit.Bukkit
+import org.bukkit.event.Cancellable
 import org.bukkit.event.Event
+import org.bukkit.event.EventPriority
 
 fun Event.call() = apply { Bukkit.getPluginManager().callEvent(this) }
+
+inline fun <reified T : Event> cancelEvent(
+    priority: EventPriority = EventPriority.NORMAL,
+    ignoreCancelled: Boolean = false,
+    register: Boolean = true
+) = cancelEventWhen<T>(priority, ignoreCancelled, register) { true }
+
+inline fun <reified T : Event> cancelEventWhen(
+    priority: EventPriority = EventPriority.NORMAL,
+    ignoreCancelled: Boolean = false,
+    register: Boolean = true,
+    crossinline condition: T.() -> Boolean
+) = listen<T>(priority, ignoreCancelled, register) {
+    if (it.condition() && it is Cancellable) it.cancel()
+}
